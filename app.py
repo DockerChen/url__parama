d213601366ad172a04cbd9c177313e092b9c5dde
woniu <18810913096@163.com>
2018-04-15 22:20:18 +0800

    add two rows

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 # coding=utf-8
+# 增加一行
+# 增加两行
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    print url_for('my_list')
+    print url_for('article', id='aaaa')
+    return 'Hello World!'
+
+
+@app.route('/list')
+def my_list():
+    return 'list'
+
+
+@app.route('/article/<id>')
+def article(id):
+    return u'你请求的参数是：%s' % id
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+# coding=utf-8
 from flask import Flask, url_for
 
 app = Flask(__name__)
